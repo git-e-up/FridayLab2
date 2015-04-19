94de9b3e457ea700dcb9ebebffabf9a47c091c35
Matt Lissner <mlissne@gmail.com>
2015-04-19 12:30:44 -0500

    more again

--- app.rb
+++ app.rb
@@ -16,31 +16,87 @@ get '/' do
 end
 
 get '/api/users' do
-All users
+  content_type :json
+  users = User.all
+  users.to_json
+end
+#All users
 get '/api/users/:id' do
-A specific user
+  content_type :json
+  user = User.find(params[:id].to_i)
+  user.to_json
+end
+#A specific user
 post '/api/users' do
-Create a new user
+  content_type :json
+  user = User.create(params[:user])
+  user.to_json
+end
+#Create a new user
 put '/api/users/:id' do
-Update an existing user
+  content_type :json
+  user = User.find(params[:id].to_i)
+  user.update(params[:user])
+  user.to_json
+end
+#Update an existing user
 patch '/api/users/:id' do
-Update an existing user
+  content_type :json
+  user = User.find(params[:id].to_i)
+  user.update(params[:user])
+  user.to_json
+end
+#Update an existing user
 delete '/api/users/:id' do
-Delete an existing user
+  content_type :json
+  user = User.delete(params[:id].to_i)
+  user.to_json
+end
+#Delete an existing user
 get '/api/locations' do
-All locations
+  content_type :json
+  locations = Location.all
+  locations.to_json
+end
+#All locations
 get '/api/locations/:id' do
-A specific location
+  content_type :json
+  location = Location.find(params[:id].to_i)
+  location.to_json
+end
+#A specific location
 post '/api/locations' do
-Create a new location
+  content_type :json
+  location = Location.create(params[:location])
+  user.to_json
+end
+#Create a new location
 put '/api/locations/:id' do
-Update an existing location
+  content_type :json
+  location = Location.find(params[:id].to_i)
+  location.update(params[:location])
+  user.to_json
+end
+#Update an existing location
 patch '/api/locations/:id' do
-Update an existing location
+  content_type :json
+  location = Location.find(params[:id].to_i)
+  location.update(params[:location])
+  user.to_json
+end
+#Update an existing location
 delete '/api/locations/:id' do
-Delete an existing location
+  content_type :json
+  location = Location.delete(params[:id].to_i)
+  location.to_json
+end
+#Delete an existing location
 get '/api/visits' do
-All visits
+  content_type :json
+  visits = Visit.all
+  visits.to_json
+end
+#All visits
 get '/api/visits/:id' do
 A specific visit
 post '/api/visits' do
